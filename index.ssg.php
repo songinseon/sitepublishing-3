b5ae2374b7a3077d4f064a0b2cce649160aee77d
songinseon <songsun95@naver.com>
2021-09-06 16:02:43 +0900

    사이트 옮기기

---
+++ index.ssg.php
@@ -0,0 +1,250 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>varoh site publishing</title>
+</head>
+<body>
+
+<!-- 이미지 확대 -->
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+<link rel="stylesheet" href="./css/index.css">
+
+<!-- 헤더 -->
+<header class="top">
+  <div class="flex height-full">
+    <div class="flex-grow"></div>
+    <nav class="top__menu-box-1">
+      <ul class="flex whitespace-nowrap height-full">
+        <li>
+          <a href="#" class="flex items-center justify-center height-full whitespace-nowrap">
+            <span>LOGIN</span>
+            <span>
+              <i class="fas fa-sort-down"></i>
+            </span>
+          </a>
+        </li>
+        <li>
+          <a href="#" class="flex items-center justify-center height-full whitespace-nowrap">
+            <span>CART</span>
+            <span>
+              <i class="fas fa-shopping-cart"></i>
+            </span>
+          </a>
+        </li>
+      </ul>
+    </nav>
+    <nav class="top__go-cart height-full">
+      <a href="#" class="items-center height-full flex">0</a>
+    </nav>
+  </div>
+</header>
+
+<!-- 상단바 -->
+<header class="top-bar">
+  <div class="con h-full flex">
+    <a href="#" class="top-bar__logo flex items-end"><img src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FoJByq%2Fbtq5QUAQjK2%2FHk0Lv1YMfInLy0ShRza4m0%2Fimg.png" alt=""></a>
+    <div class="flex-grow"></div>
+    <nav class="top-bar__menu-box-2">
+      <ul class="flex height-full flex items-end">
+        <li><a href="#" class="whitespace-nowrap">VAROH STORY</a></li>
+        <li><a href="#" class="whitespace-nowrap">BH100</a>
+          <ul>
+            <li><a href="#">FEATURES</a></li>
+            <li><a href="#">SPECS</a></li>
+          </ul>
+        </li>
+        <li><a href="#" class="whitespace-nowrap">COMMUNITY</a>
+          <ul>
+            <li><a href="#">NEWS</a></li>
+            <li><a href="#">EVENT</a></li>
+            <li><a href="#">PR CENTER</a></li>
+          </ul>
+        </li>
+        <li><a href="#" class="whitespace-nowrap">SHOP</a></li>
+        <li><a href="#" class="whitespace-nowrap">SUPPORT</a>
+          <ul>
+            <li><a href="#">FAQ</a></li>
+            <li><a href="#">1:1 INQUIRE</a></li>
+            <li><a href="#">LOCATION</a></li>
+          </ul>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</header>
+
+<!-- 배너 -->
+
+<section class="top-bn">
+  <div class="img-box"><img src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FF3uK9%2Fbtq5TswzqgL%2FPkRpHCgAnedQuOSSeoYR20%2Fimg.jpg" alt=""></div>
+  <div class="top-bn-text">
+    <div class="con">
+    <div class="whitespace-nowrap">BH PORTABLE<br>BLUETOOTH SPEAKER.</div>
+    <div class="whitespace-nowrap">가볍지만 크고 신나게,<br>Varoh와 함께라면.</div>
+    <div class="flex">
+      <nav class="top-bn-menu-box">
+      <ul class="flex">
+        <li><a href="#" class="whitespace-nowrap">SHOP NOW</a></li>
+        <li><a href="#" class="whitespace-nowrap">LOCATION NOW</a></li>
+      </ul>
+    </nav>
+    </div>
+  </div>
+  </div>
+</section>
+
+<!-- <div class="top-bn-text">
+  <div class="con">
+    <div class="whitespace-nowrap">BH PORTABLE<br>BLUETOOTH SPEAKER.</div>
+    <div class="whitespace-nowrap">가볍지만 크고 신나게,<br>Varoh와 함께라면.</div>
+    <div class="flex">
+      <nav class="top-bn-menu-box">
+      <ul class="flex">
+        <li><a href="#" class="whitespace-nowrap">SHOP NOW</a></li>
+        <li><a href="#" class="whitespace-nowrap">LOCATION NOW</a></li>
+      </ul>
+    </nav>
+    </div>
+  </div>
+</div> -->
+
+
+<!-- 배너 2 -->
+<div class="bn-2__text">
+  <div class="con">
+   <div>PREMIUM SOUND</div>
+    <div>VAROH</div>
+  </div>
+</div>
+
+<!-- <section class="bn-2__img">
+  <div class="con">
+    <img src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FJwu7y%2Fbtq6mBu8JwQ%2FHYV7mfOSUpeUPcC4ohoO70%2Fimg.jpg" alt="">
+    <img src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FIASgP%2Fbtq6pIz7oVz%2FL2j2EnXtzaufGm1j62sZCk%2Fimg.jpg" alt="">
+    <img src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FQgSYF%2Fbtq6mHIrs90%2F4KuTtBapqqHcsAGXAay4G0%2Fimg.jpg" alt="">
+  </div>
+</section> -->
+<section class="bn-2__img">
+  <div class="con">
+    <div class="work-list-box">
+      <ul>
+        <li>
+          <div>
+            <div class="img-box">
+              <div class="bg"></div>
+              <div class="addi">
+                <ul>
+                  <li>GET THE LASTEST NEWS</li>
+                  <li>VAROH NEWS</li>
+                  <li>VIEW</li>
+                </ul>
+              </div>
+              <img src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FJwu7y%2Fbtq6mBu8JwQ%2FHYV7mfOSUpeUPcC4ohoO70%2Fimg.jpg" alt="">
+            </div>
+          </div>
+        </li>
+        <li>
+          <div>
+            <div class="img-box">
+              <div class="bg"></div>
+              <div class="addi">
+                <ul>
+                  <li>PORTABLE SPEAKER</li>
+                  <li>NOW, PLAY BH100
+<br>YOUR WAY.</li>
+                  <li>EXPLORE</li>
+                </ul>
+              </div>
+              <img src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FIASgP%2Fbtq6pIz7oVz%2FL2j2EnXtzaufGm1j62sZCk%2Fimg.jpg" alt="">
+            </div>
+          </div>
+        </li>
+        <li>
+          <div>
+            <div class="img-box">
+              <div class="bg"></div>
+              <div class="addi">
+                <ul>
+                  <li>FIND ANSWER</li>
+                  <li>FAQ</li>
+                  <li>VIEW</li>
+                </ul>
+              </div>
+              <img src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FQgSYF%2Fbtq6mHIrs90%2F4KuTtBapqqHcsAGXAay4G0%2Fimg.jpg" alt="">
+            </div>
+          </div>
+        </li>
+      </ul>
+    <div>
+  </div>
+</section>
+
+<section class="section-2">
+  <div class="img-box"><img src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FlsQUU%2Fbtq6V3X145u%2FIHyp9PZ3j4WJYZ7pv2PHtK%2Fimg.jpg" alt=""></div>
+  <div class="section-2_text">
+    WIRELESS<br>  
+MAKE A<br>
+CONNECTION.
+  </div>
+  <div class="section-2_text_2">
+    눈에 보이는 컴팩트한 사이즈 안에 숨겨진 사운드,<br> 
+Varoh 스피커는 이제까지 경험하지 못했던 부드럽고 강력한 사운<br>드를 선물합니다.
+  </div>
+  <div class="section-2_text_3">
+    <ul>
+      <li><a href="#">SHOP NOW</a>
+    </li>
+      <li>
+        <a href="#">VIEW DETAIL</a>
+      </li>
+    </ul>
+  </div>
+  <div class="text-box">
+    <div class="img-box"><img src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FspD1v%2Fbtq6WiARvQZ%2FNTEBjnEgHST2glXBpy4pPK%2Fimg.png" alt=""></div>
+</section>
+  
+<!-- 푸터 -->
+<section class="putter">
+  <div class="putter_txt1">
+    <ul>
+      <li><a href="#">FIND STORE</a></li>
+      <li><a href="#">SITE MAP</a></li>
+      <li><a href="#">이용약관</a></li>
+      <li><a href="#">개인정보 취급방침</a></li>
+      <li><a href="#">2016 VAROH KOREA. ALL RIGHTS RESERVED</a></li>
+    </ul>
+  </div>
+  <div class="putter_txt2">
+    <ul>
+      <li><a href=""><img src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FoJByq%2Fbtq5QUAQjK2%2FHk0Lv1YMfInLy0ShRza4m0%2Fimg.png" alt=""></a></li>
+      <li>
+        <ul>
+          <li>
+            <ul>
+              <li>비에취어쿠스텔㈜</li>
+              <li>고객센터 1522-2885</li>
+              <li>본사 : 서울시 관악구 쑥고개로 21 (신림동) (우)08783</li>
+              <li>고객센터 : 경기도 수원시 영통구 신원로 105 (우)16678</li>
+              <li>대표자 이기헌</li>
+            </ul>
+          </li>
+          <li>
+            <ul>
+              <li>개인정보관리책임자 김윤호 (yhkim@bhacoustel.com)</li>
+              <li>사업자등록번호 112-81-34398</li>
+              <li>통신판매업신고 제2016-서울관악-0057호 [사업자정보확인]</li>
+              <li>Contact varoh@bhacoustel.com</li>
+            </ul>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+</section>
+  
+</body>
+</html>
